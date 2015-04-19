496698f439bbd13d5134c4d8868ff78c4f6c267a
jharvey <johannaaharvey@gmail.com>
2015-04-19 14:00:17 -0500

    writing functions

--- Sunday_pm-gapminder.R
+++ Sunday_pm-gapminder.R
@@ -5,4 +5,38 @@
 
 
 #read in data as tab-delimited
-gap.in <- read.table("output/combined_gapMinder.tsv", sep= "\t") 
+gap.in <- read.table("output/combined_gapMinder.tsv", sep= "\t", header=TRUE) 
+
+#gives you 
+#for (number in 1:10){
+#    number.out <- number + 10  
+#}
+
+
+#gives you the first 10 rows
+for (number in 1:10) {
+  print(gap.in[number,])
+}
+
+#nested for loops
+for (row.number in 1:10){
+  for (col.number in 1:5) {
+    print(gap.in[row.number,col.number])
+  }
+}
+
+gap.in[1:10,1:5]
+
+#writing functions in r, = a set of code that does one thing, give function a name by storing it into an object (add.me)
+add.me <- function(x, y){
+  x + y
+}
+#to run function
+add.me(3,4)
+
+
+
+
+
+
+
